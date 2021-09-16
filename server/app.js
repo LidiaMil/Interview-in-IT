e4f8529f4293c0442f2d3c8b42e95417e1c3a1ec
Lida <lidamilos@rambler.ru>
2021-09-16 10:41:57 +0300

    app.js base

--- server/app.js
+++ server/app.js
@@ -3,36 +3,23 @@ const express = require('express');
 const morgan = require('morgan');
 const hbs = require('hbs');
 const path = require('path');
-//session
 const redis = require('redis');
 const session = require('express-session');
 let RedisStore = require('connect-redis')(session);
-let redisClient = redis.createClient()
 
-const PORT = process.env.PORT || 3000;
+let redisClient = redis.createClient()
+const cors = require("cors");
+
 const app = express();
 
-// тут подключаем файлики
+const PORT = process.env.PORT ?? 3000;
 
-
-app.set('view engine', 'hbs');
-
-
-//session middleware
-
-app.use(express.urlencoded({extended:true}));
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const quoteAPI = require('quote-indo');
 
-(async () => {
-    const query = 'bucin'
-    const quote = await quoteAPI.Quotes(query);
 
-    console.log(quote);
-})()
-
-app.listen(PORT, ()=> {
-  console.log('Server start on ', PORT)
-})
+app.listen(PORT, () => {
+  console.log(`success`);
+});
