745fef56417e9f5e359e810a3ee10f5ed7f946e6
Сергей <nat29011985@gmail.com>
2021-09-22 13:49:10 +0300

    pass

--- client/src/components/NewInterview/NewInterview.jsx
+++ client/src/components/NewInterview/NewInterview.jsx
@@ -21,18 +21,18 @@ import { useParams } from 'react-router';
 import Input from '../Input/Input'
 
 export default function BasicTextFields() {
-  const[add,setAdd]=useState(true)
+  const [add, setAdd] = useState(true)
   const dispatch = useDispatch()
   let history = useHistory();
   const [cat, setCat] = useState("")
   const [company, setCompany] = useState("")
   const [newForm, setNewForm] = useState([{ name: '0' }])
-  const [title, setTitle] = useState()
-  const [description, setDescription] = useState()
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
   const { id } = useParams()
   const myInterviews = useSelector(state => state.myInterviews)
   const data = myInterviews.filter(e => e.id === Number(id))[0]
-  // console.log("======", data)
+  // console.log("======", data.name)
 
   const [level, setLevel] = useState("")
   const [news, setNews] = useState(null)
@@ -40,9 +40,10 @@ export default function BasicTextFields() {
   const org = useSelector((state) => state.org)
   const lang = useSelector((state) => state.lang)
   useEffect(() => {
-    // data = myInterviews.figitlter(e => e.id === Number(id))[0]
     if (data) {
       setLevel(data.level)
+      setDescription(data.description)
+      setTitle(data.name)
     }
   }, [data])
 
@@ -68,11 +69,11 @@ export default function BasicTextFields() {
         questionsWITHlang: input_data,
         company_id: company,
       }))
-      setAdd(null)
-      setTimeout(()=>{
-        history.push("/")
-      }, 5000);
-      
+    setAdd(null)
+    setTimeout(() => {
+      history.push("/")
+    }, 5000);
+
   }
   const editInterview = (event) => {
     event.preventDefault()
@@ -93,83 +94,85 @@ export default function BasicTextFields() {
 
   return (
     <Paper sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1, alignItems: 'center' }}>
- <h3>{id ? "Редактировать собеседование" : "Создать собеседование"}</h3>
-      {add?
-      <Grid container spacing={3}>
-      <form onSubmit={id ? editInterview : handleSubmitAdd}>
-        <Box sx={{ minWidth: 250 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Company</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={company}
-              label="Company"
-              onChange={(event) => {
-                console.log(event.target.value)
-                setCompany(event.target.value);
-              }}
-            >
-              {org.map((item, index) => <MenuItem value={item.id}>{item.id}.{item.title}</MenuItem>)}
-            </Select>
-          </FormControl>
-        </Box>
-        {/* <Box sx={{ minWidth: 500 }}>
-          <TextField id="outlined-basic" label="Level" variant="outlined" onChange={levelAdd} />
-        </Box> */}
-        <Box sx={{ minWidth: 500 }}>
-            {id ? <TextField id="outlined-basic" label="Level" variant="outlined" onChange={levelAdd} value={level} /> :
-              <TextField id="outlined-basic" label="Level" variant="outlined" onChange={levelAdd} />
-            }
-          </Box>
+      <h3>{id ? "Редактировать собеседование" : "Создать собеседование"}</h3>
+      {add ?
+        <Grid container spacing={3}>
+          <form onSubmit={id ? editInterview : handleSubmitAdd}>
+            <Box sx={{ minWidth: 250 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Company</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={company}
+                  label="Company"
+                  onChange={(event) => {
+                    // console.log(event.target.value)
+                    setCompany(event.target.value);
+                  }}
+                >
+                  {org.map((item, index) => <MenuItem value={item.id}>{item.id}.{item.title}</MenuItem>)}
+                </Select>
+              </FormControl>
+            </Box>
+
+            <Box sx={{ minWidth: 500 }}>
+              {id ? <TextField id="outlined-basic" label="Level" variant="outlined" onChange={levelAdd} value={level} /> :
+                <TextField id="outlined-basic" label="Level" variant="outlined" onChange={levelAdd} />
+              }
+            </Box>
+            {/* data.Organizations[0].title */}
+
+            <Box sx={{ minWidth: 250 }}>
+              {id ? <TextField id="outlined-basic" label="Title" variant="outlined" onChange={titleAdd} value={title}/> :
+                <TextField id="outlined-basic" label="Title" variant="outlined" onChange={titleAdd} />}
+            </Box>
+
+            <Box sx={{ minWidth: 500 }}>
+              {id ? <TextField id="outlined-basic" label="Description" variant="outlined" onChange={descriptionAdd} value={description} /> :
+                <TextField id="outlined-basic" label="Description" variant="outlined" onChange={descriptionAdd} />
+              }
+            </Box>
 
 
-        <Box sx={{ minWidth: 250 }}>
-          <TextField id="outlined-basic" label="Title" variant="outlined" onChange={titleAdd} />
-        </Box>
-        <Box sx={{ minWidth: 500 }}>
-          <TextField id="outlined-basic" label="Description" variant="outlined" onChange={descriptionAdd} />
-        </Box>
-        
+            <Box sx={{ minWidth: 250 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Categorey</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={cat}
+                  label="Organization"
+                  onChange={(event) => {
+                    setCat(event.target.value);
+                  }}
+                >
+                  {categories.map((item, index) => <MenuItem value={item.id}>{item.id}.{item.categorey}</MenuItem>)}
+                </Select>
+              </FormControl>
+            </Box>
 
-        <Box sx={{ minWidth: 250 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Categorey</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={cat}
-              label="Organization"
-              onChange={(event) => {
-                setCat(event.target.value);
-              }}
-            >
-              {categories.map((item, index) => <MenuItem value={item.id}>{item.id}.{item.categorey}</MenuItem>)}
-            </Select>
-          </FormControl>
-        </Box>
+            <Box sx={{ minWidth: 500 }}>
+              {newForm.map((el, i) => <Input key={el.name} index={el.name} lang={lang} />)}
+              <Button onClick={() => setNewForm([...newForm, { name: `${newForm.length}` }])} variant="contained">
+                Добавить вопрос
+              </Button>
+            </Box>
 
-        <Box sx={{ minWidth: 500 }}>
-          {newForm.map((el, i) => <Input key={el.name}  index={el.name} lang={lang}/>)}
-          <Button onClick={() => setNewForm([...newForm, {name: `${newForm.length}`}])} variant="contained">
-          Добавить вопрос
-          </Button>
-        </Box>
-
-        <Stack spacing={2} direction="row">
-        <Button type="submit" variant="contained">{id ? "Сохранить изменения" : "Создать"}</Button>
-        </Stack>
-      </form>
-    </Grid>
-     : <>
-     <h1>Запись успешно добавленна</h1>
-     <div>
-     <a href='/'>перейти в личный кабинет</a> 
-     </div>
-     <div>
-     <a href='/profile'>На главную</a> 
-     </div>
-      </>}
+            <Stack spacing={2} direction="row">
+              <Button type="submit" variant="contained">{id ? "Сохранить изменения" : "Создать"}</Button>
+            </Stack>
+          </form>
+        </Grid>
+        : <>
+          <h1>Запись успешно добавленна</h1>
+          <div>
+            <a href='/'>перейти в личный кабинет</a>
+          </div>
+          <div>
+            <a href='/profile'>На главную</a>
+          </div>
+        </>}
     </Paper>
   );
 }
